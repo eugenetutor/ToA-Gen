2a93308a2e4c7f16c59d2c8e27f966cb03753379
demenkoeugene <mimosa.01-purgers@icloud.com>
2023-05-06 19:57:22 +0300

    lesson7(full)

---
+++ src/DataStructure/TestClass.java
@@ -0,0 +1,14 @@
+package DataStructure;
+
+public class TestClass {
+    public static void main(String[] args) {
+        //dynamic array
+       int[] dynArray;
+       dynArray = new int[5];
+
+       dynArray[0] = 1;
+       dynArray[2] = 3;
+       //static array
+       int[] staticArray = {1,2,4,4,5,6,6,7,8,8,9};
+    }
+}
